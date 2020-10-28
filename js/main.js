4fdb5538be8c39eca76b3871302e3917c03a169a
Fabio <fabio.mezzina.09@gmail.com>
2020-10-28 15:49:37 +0100

    JS mostro menù

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-  
-  
+  // reference
+  var actions = $('.with-dropdown a');
+  var menus = $('.dropdown-menu');
+  // click su elemento del menù
+  actions.click( function() {
+    // ref al menù relativo all'elemento cliccato
+    var actualMenu = $(this).next('.dropdown-menu');
+    // nascondo tutti i menù tranne quello dell'elemento cliccato
+    menus.not(actualMenu).hide();
+    // apro o chiudo in menù cliccato
+    actualMenu.toggle();
+  });
 
 }); // <-- end doc ready
