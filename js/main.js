48ea7fc565d2fd306037000cd1d8e9d00eb90edf
Fabio <fabio.mezzina.09@gmail.com>
2020-10-28 18:16:47 +0100

    mod gestione focus da JS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   // reference
   var actions = $('.with-dropdown a');
   var menus = $('.dropdown-menu');
+  var links = $('nav a');
   // click su elemento del menù
   actions.click( function(event) {
     // ref al menù relativo all'elemento cliccato
@@ -13,8 +14,13 @@ $(document).ready(function () {
     // evita che il click dei dropdown venga visto come un click sul document
     event.stopPropagation();
   });
+  // gestisco il focus sui link
+  links.click( function() {
+    $(this).addClass('color-black');
+    links.not(this).removeClass('color-black');
+  });
   // chiudo il menù aperto se clicco all'esterno
-  $(document).click(function(){
+  $(document).click(function(e){
     $(menus).hide();
   });
 }); // <-- end doc ready
