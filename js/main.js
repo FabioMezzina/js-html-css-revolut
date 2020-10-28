52986b54c3b187a5cf6552627fedf986166596d8
Fabio <fabio.mezzina.09@gmail.com>
2020-10-28 17:17:14 +0100

    nascondo dropdown con click all'esterno

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ $(document).ready(function () {
   var actions = $('.with-dropdown a');
   var menus = $('.dropdown-menu');
   // click su elemento del menù
-  actions.click( function() {
+  actions.click( function(event) {
     // ref al menù relativo all'elemento cliccato
     var actualMenu = $(this).next('.dropdown-menu');
     // nascondo tutti i menù tranne quello dell'elemento cliccato
     menus.not(actualMenu).hide();
-    // apro o chiudo in menù cliccato
+    // apro o chiudo il menù cliccato
     actualMenu.toggle();
+    // evita che il click dei dropdown venga visto come un click sul document
+    event.stopPropagation();
+  });
+  // chiudo il menù aperto se clicco all'esterno
+  $(document).click(function(){
+    $(menus).hide();
   });
 }); // <-- end doc ready
